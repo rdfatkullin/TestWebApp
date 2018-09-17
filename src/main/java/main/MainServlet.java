c19c57e1d088c5efef02f41e43ef4a900c0d1c04
Riyaz Fatkullin <rdfatkullin@gmail.com>
2018-09-17 17:44:58 +0300

    Edited web.xml, folder structure

--- src/main/java/main/MainServlet.java
+++ src/main/java/main/MainServlet.java
@@ -4,11 +4,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MainServlet extends HttpServlet{
 
     public void doGet(HttpServletRequest request,
-                      HttpServletResponse response) throws ServletException {
+                      HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>Hello Servlet Get</h1>");
+        out.println("</body>");
+        out.println("</html>");
         response.setStatus(HttpServletResponse.SC_OK);
     }
 
